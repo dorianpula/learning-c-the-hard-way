b789076ade6d49a50e53cccfb1b4bd3407c7f0e1
Dorian Pula <dorian.pula@amber-penguin-software.ca>
2013-08-02 08:46:04 -0400

    Further work on exercise 17.

--- pointers_structs_exec/simple-db-ex17.c
+++ pointers_structs_exec/simple-db-ex17.c
@@ -60,6 +60,61 @@ struct Connection *Database_open(const char *filename, char mode)
 	struct Connection *conn = malloc(sizeof(struct Connection));
 	if(!conn) die("Memory error");
 
-	// TODO Carry on from here...
+	// Attempt to initialize the database in memory.
+	conn->db = malloc(sizeof(struct Database));
+	if (!conn->db) die("Memory error when initializing database.");
 
+	// Open the file in write or read depending on the mode.
+	if (mode == 'c') {
+		conn->file = fopen(filename, "w");
+	} else {
+		conn->file = fopen(filename, "r+");
+
+		if (conn->file) {
+			Database_load(conn);
+		}
+	}
+
+	if (!conn->file) die("Failed to open the file.");
+
+	return conn;
 }
+
+// Close the database Connection
+void Database_close(struct Connection *conn)
+{
+	if (conn) {
+		if (conn->file) fclose(conn->file);
+		if (conn->db) free(conn->db);
+		free(conn);
+	}
+}
+
+// Write to the database
+void Database_write(struct Connection *conn)
+{
+	// Rewinds the file.
+	rewind(conn->file);
+
+	if ret_code = fwrite(conn->db, sizeof(struct Database), 1, conn->file);
+	if (ret_code != 1) die("Failed to write database.");
+
+	// Flush the remaining user space buffers into the file.
+	ret_code = fflush(conn->file);
+	if (ret_code == -1) die("Cannot flush database.");
+}
+
+// Create the database.
+void Database_create(struct Connection *conn)
+{
+	int i = 0;
+
+	for (i = 0; i < MAX_ROWS; i++){
+		// Makes a prototype and initializes it.
+		struct Address addr = {.id = i, .set = 0};
+		// Then assign it.
+		conn->db->rows[i] = addr;
+	}
+}
+
+// TODO: Continue on from here...

aa2b85d286c779fe54a97b939ae94ec32e795991
Dorian Pula <dorian.pula@amber-penguin-software.ca>
2013-08-06 16:01:32 -0400

    Carrying on with implementing a simple game using a simple object system. :)

--- pointers_structs_exec/ex19-simple-object-game/object.c
+++ pointers_structs_exec/ex19-simple-object-game/object.c
@@ -41,5 +41,28 @@ int Object_attack(void *self, int damage)
 
 void *Object_new(size_t size, Object proto, char *description)
 {
-	// TODO Complete me.
+	// Setup the default functions in case they are not set...
+	if(!proto.init) proto.init = Object_init;
+	if(!proto.describe) proto.describe = Object_describe;
+	if(!proto.destroy) proto.destroy = Object_destroy;
+	if(!proto.attack) proto.attack = Object_attack;
+	if(!proto.move) proto.move = Object_move;
+
+	// Seems weird to make a struct of one size, then point a different pointer
+	// at it to "cast" it.
+	Object *el = calloc(1, size);
+	*el = proto;
+
+	// Copy the description
+	el->description = strdup(description);
+
+	// Initialize this to whatever init we were given.
+	if (!el->init(el)) {
+		// Crap not properly init'ed.
+		el->destroy(el);
+		return NULL;
+	} else {
+		// Done.  We make an object of any type.
+		return el;
+	}
 }
